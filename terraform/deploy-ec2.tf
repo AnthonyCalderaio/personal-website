afcfd3858234cc438c5e67e4f1738671b06404f7
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-05-04 22:45:07 -0400

    cool

--- terraform/deploy-ec2.tf
+++ terraform/deploy-ec2.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "example" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
   key_name      = "04_23_2024_key"
-  security_groups = ["allow_ssh","allow_outbound", aws_security_group.example_sg2.name]
+  security_groups = ["allow_ssh","allow_outbound", "allow-outbound-1"]
 
   tags = {
     Name = "Personal Website"
