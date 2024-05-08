c5144db663c2e7e3e120db97252092b62a3a964c
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-05-07 23:53:19 -0400

    use this

--- terraform/scripts/build_script.sh
+++ terraform/scripts/build_script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 #1) Install node:
-rm -rf /etc/yum.repos.d/nodesource-el* && 
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash - && 
-yum install nodejs --enablerepo=nodesource && 
-sudo dnf install nodejs -y && 
+sudo rm -rf /etc/yum.repos.d/nodesource-el*
+
+# Install Node.js repository for YUM
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+
+# Install Node.js using YUM
+sudo yum install nodejs -y
 
 #2) install http-server
 sudo npm install -g http-server &&
