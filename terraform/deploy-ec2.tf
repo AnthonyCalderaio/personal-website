e0cecd7077a432603640020a44a5d6c4e7a3535d
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-05-08 00:22:35 -0400

    try this

--- terraform/deploy-ec2.tf
+++ terraform/deploy-ec2.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "example" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
   key_name      = "04_23_2024_key"
-  security_groups = ["allow_ssh","allow_outbound", aws_security_group.Personal_Website_Inbound.name]
+  security_groups = ["allow_ssh","allow_outbound", "Personal_Website_Inbound"]
 
   tags = {
     Name = "Personal Website"
