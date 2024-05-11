efa3cb37619da25c43c230e8082cde4a0e564e93
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-05-11 16:50:21 -0400

    annoyed

--- terraform/deploy-ec2.tf
+++ terraform/deploy-ec2.tf
@@ -27,26 +27,26 @@ provider "aws" {
 
 ########################
 # Define security group for HTTP server
-resource "aws_security_group" "http_server_sg" {
-  name        = "http-server-sg"
-  description = "Security group for HTTP server"
+# resource "aws_security_group" "http_server_sg" {
+ # name        = "http-server-sg"
+ # description = "Security group for HTTP server"
 
   # Inbound rule for HTTP traffic
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from any source IP address
-  }
+ # ingress {
+   # from_port   = 80
+   # to_port     = 80
+   # protocol    = "tcp"
+   # cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from any source IP address
+  #}
 
   # Outbound rule to allow all traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # All protocols
-    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic to any destination IP address
-  }
-}
+ # egress {
+   # from_port   = 0
+   # to_port     = 0
+   # protocol    = "-1"  # All protocols
+  #  cidr_blocks = ["0.0.0.0/0"]  # Allow traffic to any destination IP address
+ # }
+#}
 ########################
 
 # Create the EC2 instance and associate it with the new security group
